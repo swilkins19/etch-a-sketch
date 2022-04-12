b06986df210e8fc92af96e33992131a19f4c5506
Sean Wilkinson <s7wilkinson@gmail.com>
2022-04-12 15:11:05 -0500

    Reset and resize canvas

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 function setUpGrid(rowSize) {
     let grid = document.querySelector(".grid");
+    while(grid.firstChild) {   //clear current grid
+        grid.removeChild(grid.firstChild);
+    }
+
     for(let j=0; j<rowSize; j++) {
         let row = document.createElement("div");
         row.classList.add("row");
@@ -17,4 +21,14 @@ function hover() {
     this.style.backgroundColor = 'blue';
 }
 
+function reset() {
+    let rowSize = prompt("How many squares per side would you like your new drawing to be? (max 100)", "16");
+    while(!rowSize || isNaN(parseInt(rowSize))) {
+        rowSize = prompt("You did not enter a valid number, please use integers only.\nHow many squares per side would you like your new drawing to be? (max 100)", "16");
+    }
+    setUpGrid(parseInt(rowSize));
+}
+
+const resetBtn = document.querySelector('#reset-btn');
+resetBtn.addEventListener('click', reset);
 setUpGrid(16);
